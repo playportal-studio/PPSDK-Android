edab4d07c2b783cd4029f8d057e77c8cd134fe28
Gary Baldwin <g11baldwin@gmail.com>
2018-06-05 11:13:37 -0400

    Replaced MainActivity with an “ExampleMainActivity”

--- app/src/main/java/com/dynepic/ppsdk_android/ExampleMainActivity.java
+++ app/src/main/java/com/dynepic/ppsdk_android/ExampleMainActivity.java
@@ -3,7 +3,7 @@ package com.dynepic.ppsdk_android;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class ExampleMainActivity extends AppCompatActivity {
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
