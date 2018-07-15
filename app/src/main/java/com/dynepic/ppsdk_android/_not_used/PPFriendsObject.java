462ac5be93438bb34a825fbb9e4db1852fa65ad0
presadb <eprice@netrist.com>
2018-07-15 15:46:59 -0400

    unused resources
    added unused resources for later refactoring

---
+++ app/src/main/java/com/dynepic/ppsdk_android/_not_used/PPFriendsObject.java
@@ -0,0 +1,5 @@
+package com.dynepic.ppsdk_android._not_used;//package com.dynepic.ppsdk_android.REFACTOR_IN_PROGRESS;
+//
+//class PPFriendsObject {
+//
+//}
---
+++ app/src/main/java/com/dynepic/ppsdk_android/_not_used/PPUserService_old.java
@@ -0,0 +1,26 @@
+package com.dynepic.ppsdk_android._not_used;//package com.dynepic.ppsdk_android.REFACTOR_IN_PROGRESS;
+//
+//import android.content.Context;
+//
+//import com.dynepic.ppsdk_android.models.PPUserObject;
+//import com.dynepic.ppsdk_android.utils._UserPrefs;
+//
+//public class PPUserService_old {
+//
+//	private Context CONTEXT;
+//	private _UserPrefs userPrefs;
+//
+//	public PPUserService_old(Context context){
+//		CONTEXT = context;
+//	}
+//
+//	public Boolean hasUser(){
+//		userPrefs = new _UserPrefs(CONTEXT);
+//		return userPrefs.exists();
+//	}
+//
+//	public void storeUser(PPUserObject userObject){
+//
+//	}
+//
+//}
