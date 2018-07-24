1cbd98a00e938d8b888be4463d6275b904cb5b9c
Gary Baldwin <g11baldwin@gmail.com>
2018-07-24 13:22:12 -0400

    Callback fnx interface
    - define callback from methods invoking web API (http) calls, since these are all performed asynchronously

--- ppsdk_android/src/main/java/com/dynepic/ppsdk_android/utils/_CallbackFunction.java
+++ ppsdk_android/src/main/java/com/dynepic/ppsdk_android/utils/_CallbackFunction.java
@@ -1,4 +1,8 @@
 package com.dynepic.ppsdk_android.utils;
 
-public class _CallbackFunction {
+import android.support.annotation.Nullable;
+
+public interface _CallbackFunction {
+	public void cbf(@Nullable String bucketName, @Nullable String key, @Nullable String data, @Nullable String error);
 }
+
