3204f267d5ddecf5c4d6a55b470b5d6b3e8a1944
Gary Baldwin <g11baldwin@gmail.com>
2018-07-26 14:49:21 -0400

    Modified structure to support callbacks
    - callbacks are specific to the needs of the provider/consumer
    - there’s a default callback method, but the practice is to @Override and define what is needed in various instances

--- ppsdk_android/src/main/java/com/dynepic/ppsdk_android/utils/_CallbackFunction.java
+++ ppsdk_android/src/main/java/com/dynepic/ppsdk_android/utils/_CallbackFunction.java
@@ -1,8 +1,22 @@
 package com.dynepic.ppsdk_android.utils;
 
 import android.support.annotation.Nullable;
+import android.util.Log;
+
+import com.dynepic.ppsdk_android.models.User;
+import com.google.gson.JsonObject;
+
+import java.util.ArrayList;
 
 public interface _CallbackFunction {
-	public void cbf(@Nullable String bucketName, @Nullable String key, @Nullable String data, @Nullable String error);
+	public interface _Data {
+		//		public void f(String bucketName, @Nullable String key, @Nullable String data, @Nullable String error);
+		public void f(String bucketName, @Nullable String key, @Nullable JsonObject data, @Nullable String error);
+	}
+
+	public interface _Friends {
+		public void f(@Nullable ArrayList<User> friends, @Nullable String error);
+	}
 }
+
 
