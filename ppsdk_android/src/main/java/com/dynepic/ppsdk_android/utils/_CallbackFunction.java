6443445340aad605f8f402866ac23f56ba7ad65e
Gary Baldwin <g11baldwin@gmail.com>
2018-07-27 10:23:16 -0400

    cleanup
    - no functional change

--- ppsdk_android/src/main/java/com/dynepic/ppsdk_android/utils/_CallbackFunction.java
+++ ppsdk_android/src/main/java/com/dynepic/ppsdk_android/utils/_CallbackFunction.java
@@ -1,7 +1,6 @@
 package com.dynepic.ppsdk_android.utils;
 
 import android.support.annotation.Nullable;
-import android.util.Log;
 
 import com.dynepic.ppsdk_android.models.User;
 import com.google.gson.JsonObject;
@@ -10,7 +9,6 @@ import java.util.ArrayList;
 
 public interface _CallbackFunction {
 	public interface _Data {
-		//		public void f(String bucketName, @Nullable String key, @Nullable String data, @Nullable String error);
 		public void f(String bucketName, @Nullable String key, @Nullable JsonObject data, @Nullable String error);
 	}
 
