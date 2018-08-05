2ea9636e214f04eeffc7bbfd5d62c4de7e37da8b
Gary Baldwin <g11baldwin@gmail.com>
2018-08-05 18:45:01 -0400

    remove “ref” from data read/writes
    - was used for associating delegate calls with read/write ops
    - moved to lambdas exclusively as Google is requiring Android 26 or later after September 2018

--- ppsdk_android/src/main/java/com/dynepic/ppsdk_android/utils/_DataService.java
+++ ppsdk_android/src/main/java/com/dynepic/ppsdk_android/utils/_DataService.java
@@ -27,7 +27,7 @@ public class _DataService {
 		webApi = w;
 	}
 
-	public void createBucket(int uniqueRef, String bucketName, ArrayList<String> bucketUsers, Boolean isPublic, Context CONTEXT, _CallbackFunction._Data cb) {
+	public void createBucket(String bucketName, ArrayList<String> bucketUsers, Boolean isPublic, Context CONTEXT, _CallbackFunction._Data cb) {
 
 		com.dynepic.ppsdk_android.utils._DevPrefs devPrefs = new com.dynepic.ppsdk_android.utils._DevPrefs(CONTEXT);
 
@@ -45,22 +45,22 @@ public class _DataService {
 				public void onResponse(Call<JsonObject> call, Response<JsonObject> response) {
 					if((response.code() == 200) || (response.code() == 409))
 						Log.d("createBucket response: ", String.valueOf(response.body()));
-						cb.f(uniqueRef, response.body(), null);
+						cb.f(response.body(), null);
 					}
 				@Override
 				public void onFailure(Call<JsonObject> call, Throwable t) {
 					Log.e("createBucket error:", "failed with " + t);
-					cb.f( uniqueRef, null, t.getMessage());
+					cb.f(null, t.getMessage());
 				}
 			});
 		} else {
-			cb.f(uniqueRef, null, "Error: failed to create bucket - bucket name 'null'");
+			cb.f(null, "Error: failed to create bucket - bucket name 'null'");
 		}
 	}
 
-	public void readBucket(int uniqueRef, String bucketName, String key, Context CONTEXT, _CallbackFunction._Data cb)
+	public void readBucket(String bucketName, String key, Context CONTEXT, _CallbackFunction._Data cb)
 	{
-		Log.d("readBucket: ", bucketName + " and key:" + key + " unique Ref:" + uniqueRef);
+		Log.d("readBucket: ", bucketName + " and key:" + key);
 		Boolean readSuccess = true;
 		com.dynepic.ppsdk_android.utils._DevPrefs devPrefs = new com.dynepic.ppsdk_android.utils._DevPrefs(CONTEXT);
 
@@ -78,16 +78,16 @@ public class _DataService {
 						try {
 						JsonObject jdata = response.body().getAsJsonObject("data");
 						Log.d("value@key: ", key + " : "+ jdata.get(key).toString());
-						cb.f(uniqueRef, jdata.getAsJsonObject(key), null);
+						cb.f(jdata.getAsJsonObject(key), null);
 						} catch(Exception e) {
-							cb.f(uniqueRef, null, "Key: "+key+" not found in bucket:"+bucketName+"  Err:"+e.getMessage());
+							cb.f(null, "Key: "+key+" not found in bucket:"+bucketName+"  Err:"+e.getMessage());
 						}
 					}
 				}
 				@Override
 				public void onFailure(Call<JsonObject> call, Throwable t) {
 					Log.e("readBucket error:", "failed with " + t + " on bucket:" + bucketName);
-					cb.f(uniqueRef, null, t.getMessage());
+					cb.f(null, t.getMessage());
 				}
 			});
 		}
@@ -96,7 +96,7 @@ public class _DataService {
 
 
 
-	public void writeBucket(int uniqueRef, String bucketName, String key, JsonObject value, Boolean push, Context CONTEXT, _CallbackFunction._Data cb) {
+	public void writeBucket(String bucketName, String key, JsonObject value, Boolean push, Context CONTEXT, _CallbackFunction._Data cb) {
 		Log.d("writeBucket", "bucket: " + bucketName + " key:" + key + " value:" + value);
 		if (bucketName != null && key != null && value != null) {
 			com.dynepic.ppsdk_android.utils._DevPrefs devPrefs = new com.dynepic.ppsdk_android.utils._DevPrefs(CONTEXT);
@@ -115,18 +115,18 @@ public class _DataService {
 					if(response.code() == 200) {
 						JsonObject jdata = response.body().getAsJsonObject("data");
 						Log.d("writeBucket res: ", String.valueOf(response.body()));
-						cb.f(uniqueRef, null, null);
+						cb.f(null, null);
 					}
 				}
 
 				@Override
 				public void onFailure(Call<JsonObject> call, Throwable t) {
 					Log.e("writeBucket error:", "failed with " + t);
-					cb.f(uniqueRef, null, t.getMessage());
+					cb.f(null, t.getMessage());
 				}
 			});
 		} else {
-			cb.f(uniqueRef, null, "Error: bucket write failed - invalid parms!");
+			cb.f(null, "Error: bucket write failed - invalid parms!");
 		}
 	}
 
